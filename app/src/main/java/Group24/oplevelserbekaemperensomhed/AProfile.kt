e1f5ba2669bd0865d7755b7607b9a68d10b69ab1
JayVeeDee <jayveedees@gmail.com>
2020-10-04 15:16:58 +0200

    added two more activities that start from AEventSwiper.kt

---
+++ app/src/main/java/Group24/oplevelserbekaemperensomhed/AProfile.kt
@@ -0,0 +1,11 @@
+package Group24.oplevelserbekaemperensomhed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.aprofile)
+    }
+}
