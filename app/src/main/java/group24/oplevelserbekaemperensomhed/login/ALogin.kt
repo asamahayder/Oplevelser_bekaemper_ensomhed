e42ad28ee52e9bb4a479dd98a075cdc43726b4c2
jvd <jayveedees@gmail.com>
2020-10-30 14:16:40 +0100

    Started work on the login side of the app

---
+++ app/src/main/java/group24/oplevelserbekaemperensomhed/login/ALogin.kt
@@ -0,0 +1,12 @@
+package group24.oplevelserbekaemperensomhed.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import group24.oplevelserbekaemperensomhed.R
+
+class ALogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.alogin)
+    }
+}
