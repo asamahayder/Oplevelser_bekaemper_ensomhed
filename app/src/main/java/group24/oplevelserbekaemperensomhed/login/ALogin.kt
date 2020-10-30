da5149228a99944cf3636877f1fc8574cdd4e1e2
jvd <jayveedees@gmail.com>
2020-10-30 16:04:10 +0100

    Started implementing firebase/facebook login

--- app/src/main/java/group24/oplevelserbekaemperensomhed/login/ALogin.kt
+++ app/src/main/java/group24/oplevelserbekaemperensomhed/login/ALogin.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import group24.oplevelserbekaemperensomhed.R
 
+
 class ALogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
