ce8e76119ce863d069141869ca2f4f60cd6f3b26
Thaer91 <thaerthaer1234@gmail.com>
2021-01-15 21:03:08 +0100

    Commit.,,

--- app/src/main/java/group24/oplevelserbekaemperensomhed/search/Indstillinger.java
+++ app/src/main/java/group24/oplevelserbekaemperensomhed/search/Indstillinger.java
@@ -6,8 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
 
 import group24.oplevelserbekaemperensomhed.R;
+import group24.oplevelserbekaemperensomhed.data.LocalData;
+import group24.oplevelserbekaemperensomhed.logic.firebase.FirebaseDAO;
+import group24.oplevelserbekaemperensomhed.logic.firebase.MyCallBack;
+import group24.oplevelserbekaemperensomhed.view.login.ActivityLogin;
 
 public class Indstillinger extends AppCompatActivity {
 
@@ -16,11 +25,13 @@ public class Indstillinger extends AppCompatActivity {
     Button btn_log_ud;
     Button btn_delete_acc;
 
+    LocalData localData = LocalData.INSTANCE;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_indstillinger);
+        setContentView(R.layout.activity_settings);
 
 
         btn_vilkår = (Button) findViewById(R.id.button1);
@@ -55,6 +66,25 @@ public class Indstillinger extends AppCompatActivity {
         btn_delete_acc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                FirebaseDAO fb = new FirebaseDAO();
+                fb.deleteUser(localData.getId(), new MyCallBack() {
+                    @Override
+                    public void onCallBack(@NotNull Object object) {
+                        boolean status = (boolean) object;
+                        if (status) {
+                            FirebaseAuth auth = FirebaseAuth.getInstance();
+                            auth.getCurrentUser().delete();
+
+                            Intent intent = new Intent(getApplicationContext(), ActivityLogin.class);
+                            startActivity(intent);
+                            finish();
+
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Could not delete the user :(",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
             }
         });
