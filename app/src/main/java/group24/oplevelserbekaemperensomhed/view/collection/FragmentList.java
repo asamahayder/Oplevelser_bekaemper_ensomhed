68eb3edf0360a438a212737914a48de53ee7cc75
JayVeeDee <jayveedees@gmail.com>
2021-01-18 04:37:30 +0100

    moved around the code structure into their respective packages. Separating logic from view and categorizing them a bit

--- app/src/main/java/group24/oplevelserbekaemperensomhed/view/collection/FragmentList.java
+++ app/src/main/java/group24/oplevelserbekaemperensomhed/view/collection/FragmentList.java
@@ -8,8 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
-
 import com.google.android.material.tabs.TabLayout;
 
 import group24.oplevelserbekaemperensomhed.R;
